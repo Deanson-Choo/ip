4d99e0bf44fe35ce80a68f66e467c9cae3b72ac8
Deanson Choo <e1121432@u.nus.edu>
2025-01-23 22:15:27 +0800

    Level-0

--- src/main/java/Jeff.java
+++ src/main/java/Jeff.java
@@ -1,10 +1,10 @@
 public class Jeff {
     public static void main(String[] args) {
-        System.out.println("_____________________________________________");
+        System.out.println("____________________________________________");
         System.out.println("Hello! I'm Jeff!");
         System.out.println("What can I do for you?");
-        System.out.println("_____________________________________________");
+        System.out.println("____________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("_____________________________________________");
+        System.out.println("____________________________________________");
     }
 }
