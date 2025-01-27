94cc10e19d7c5e656a23303ed10097a0f4d3eb2a
Deanson Choo <e1121432@u.nus.edu>
2025-01-28 00:15:55 +0800

    Level-1: Echo

--- src/main/java/Jeff.java
+++ src/main/java/Jeff.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class Jeff {
     public static void main(String[] args) {
         System.out.println("____________________________________________");
         System.out.println("Hello! I'm Jeff!");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________");
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String line = in.nextLine();
+            if (line.equals("bye")) {
+                break;
+            }
+            System.out.println("____________________________________________");
+            System.out.println(line);
+            System.out.println("____________________________________________");
+        }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________");
     }
