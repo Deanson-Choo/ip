64be9b7818949d4a70095ef4e641f1fe21d616f7
Deanson Choo <e1121432@u.nus.edu>
2025-01-28 13:26:53 +0800

    Level-2: bot echoes what u add and list out added items when u request

--- src/main/java/Jeff.java
+++ src/main/java/Jeff.java
@@ -1,21 +1,46 @@
 import java.util.Scanner;
 
+
+
 public class Jeff {
+
+    public static void read_and_print() {
+        String[] items = new String[100];
+        Scanner in = new Scanner(System.in);
+        int counter = 0;
+        while (true) {
+            String line = in.nextLine();
+
+            if (line.equals("bye")) { //Exit Program
+                break;
+            }
+            if (line.equals("list")) { //List items
+                System.out.println("____________________________________________");
+                for (String item : items) {
+                    if (item != null) {
+                        System.out.println(item);
+                    }
+                    else { //Since items are sloted at the front, this means it's a null here
+                        break;
+                    }
+                }
+                System.out.println("____________________________________________");
+            }
+            else { //Add items
+                items[counter] = line;
+                counter++;
+                System.out.println("____________________________________________");
+                System.out.println("added: " + items[counter - 1]);
+                System.out.println("____________________________________________");
+            }
+        }
+    }
     public static void main(String[] args) {
         System.out.println("____________________________________________");
         System.out.println("Hello! I'm Jeff!");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________");
-        Scanner in = new Scanner(System.in);
-        while (true) {
-            String line = in.nextLine();
-            if (line.equals("bye")) {
-                break;
-            }
-            System.out.println("____________________________________________");
-            System.out.println(line);
-            System.out.println("____________________________________________");
-        }
+        read_and_print();
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________");
     }
